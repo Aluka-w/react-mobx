38350d71a720f2789887c41cee8341bbaef9ca33
Aluka-w <1103500835@qq.com>
2018-10-23 14:50:24 +0800

    mobx装饰器的配置

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { observer } from 'mobx-react';
 
-class App extends Component {
-  render() {
+@observer class App extends Component {
+  constructor (props) {
+    super(props)
+  }
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        test
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
